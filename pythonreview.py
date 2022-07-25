fe84975bb3771ca74b2906e73e918870f2518611
yairBR23-meet <109908955+yairBR23-meet@users.noreply.github.com>
2022-07-25 13:49:58 +0300

    python lab1

---
+++ pythonreview.py
@@ -0,0 +1,29 @@
+def my_function(title, description):
+  a = {"title": title, "description": description, "likes": 0, "dislikes": 0, "comments":};
+  return a;
+
+
+def like(dictionary):
+  if "likes" in dictionary.keys():
+    dictionary["likes"] = +1
+
+    return dictionary
+
+
+def dislike(dictionary):
+  if "dislikes" in dictionary.keys():
+    dictionary["dislikes"] = +1
+
+    return dictionary
+
+
+def add_comment("video, username, comment_text"):
+  if "comments" in video.keys():
+    video["username": "comment_text"]
+
+    return videocd 
+
+a = my_function(h, g)
+like(a)
+dislike(a)
+print(a) 
